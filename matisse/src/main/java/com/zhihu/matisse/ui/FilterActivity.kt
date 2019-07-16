284fde0f64fb88323e3d062eef5df662d982d923
ngocsontr <transon97uet@gmail.com>
2019-07-16 11:43:27 +0700

    [Modify] Fix bug toolbar

--- matisse/src/main/java/com/zhihu/matisse/ui/FilterActivity.kt
+++ matisse/src/main/java/com/zhihu/matisse/ui/FilterActivity.kt
@@ -24,7 +24,6 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.zhihu.matisse.Matisse
 import com.zhihu.matisse.R
-import com.zhihu.matisse.internal.entity.SelectionSpec
 import com.zomato.photofilters.FilterPack
 import com.zomato.photofilters.imageprocessors.Filter
 import com.zomato.photofilters.utils.ThumbnailCallback
@@ -36,7 +35,7 @@ import kotlinx.android.synthetic.main.filter_images.view.*
 class FilterActivity : AppCompatActivity(), ThumbnailCallback, OnViewPagerImageLoad {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(SelectionSpec.getInstance().themeId)
+        setTheme(R.style.Matisse_Zhihu)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_filter)
 
